59a5da492c68fa76e40f6e6c53abc98a43d2d2eb
wenha0 <921790476@qq.com>
2019-02-13 21:00:52 +0800

    eureka server端更新包名

--- shop-basic/shop-basic-eureka/src/main/java/com/wenhao/shop/eureka/EurekaApplication.java
+++ shop-basic/shop-basic-eureka/src/main/java/com/wenhao/shop/eureka/EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.wenhao.shop.springcloud.eureka;
+package com.wenhao.shop.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
