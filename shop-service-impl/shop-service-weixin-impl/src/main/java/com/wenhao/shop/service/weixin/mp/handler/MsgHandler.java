659450f507aab2c682fabbb9b8a92d2613cd325d
wenhao <921790476@qq.com>
2019-03-15 13:57:49 +0800

    添加验证码工具 以及设置

--- shop-service-impl/shop-service-weixin-impl/src/main/java/com/wenhao/shop/service/weixin/mp/handler/MsgHandler.java
+++ shop-service-impl/shop-service-weixin-impl/src/main/java/com/wenhao/shop/service/weixin/mp/handler/MsgHandler.java
@@ -1,5 +1,6 @@
 package com.wenhao.shop.service.weixin.mp.handler;
 
+import com.wenhao.shop.core.utils.RegexUtils;
 import com.wenhao.shop.service.weixin.mp.builder.TextBuilder;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.common.session.WxSessionManager;
@@ -7,6 +8,7 @@ import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -18,6 +20,12 @@ import static me.chanjar.weixin.common.api.WxConsts.XmlMsgType;
  */
 @Component
 public class MsgHandler extends AbstractHandler {
+
+    @Value("${mayikt.weixin.registration.code.message}")
+    private String registrationCodeMessage;
+
+    @Value("${mayikt.weixin.default.registration.code.message}")
+    private String defaultRegistrationCodeMessage;
 
     @Override
     public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage,
@@ -31,11 +39,11 @@ public class MsgHandler extends AbstractHandler {
         //当用户输入关键词如“你好”，“客服”等，并且有客服在线时，把消息转发给在线客服
         try {
             if (StringUtils.startsWithAny(wxMessage.getContent(), "你好", "客服")
-                && weixinService.getKefuService().kfOnlineList()
-                .getKfOnlineList().size() > 0) {
+                    && weixinService.getKefuService().kfOnlineList()
+                    .getKfOnlineList().size() > 0) {
                 return WxMpXmlOutMessage.TRANSFER_CUSTOMER_SERVICE()
-                    .fromUser(wxMessage.getToUser())
-                    .toUser(wxMessage.getFromUser()).build();
+                        .fromUser(wxMessage.getToUser())
+                        .toUser(wxMessage.getFromUser()).build();
             }
         } catch (WxErrorException e) {
             e.printStackTrace();
@@ -43,10 +51,26 @@ public class MsgHandler extends AbstractHandler {
 
         //TODO 组装回复消息
         //String content = "收到信息内容：" + JsonUtils.toJson(wxMessage);
-        String content = "收到信息内容：" + "谢谢";
+        //String content = "收到信息内容：" + "谢谢";
+        String fromContent = wxMessage.getContent();
+        //校验手机号
+        if (RegexUtils.checkMobile(fromContent)) {
+            //生成code
+            int registCode = registCode();
+            //替换成验证码
+            String content = registrationCodeMessage.format(registrationCodeMessage, registCode);
+            //返回code
+            return new TextBuilder().build(content, wxMessage, weixinService);
+        }
+        //返回默认消息
+        return new TextBuilder().build(defaultRegistrationCodeMessage, wxMessage, weixinService);
 
-        return new TextBuilder().build(content, wxMessage, weixinService);
+    }
 
+    //获取注册码
+    private int registCode() {
+        int registCode = (int) (Math.random() * 9000 + 1000);
+        return registCode;
     }
 
 }
