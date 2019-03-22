c5c3893e4bb2273d09743f1c7bdd4127177ce86a
wenhao <921790476@qq.com>
2019-03-22 22:46:53 +0800

    验证码

--- shop-portal/shop-portal-web/src/main/java/com/wenhao/PortalWebApplication.java
+++ shop-portal/shop-portal-web/src/main/java/com/wenhao/PortalWebApplication.java
@@ -2,9 +2,17 @@ package com.wenhao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-
-@SpringBootApplication(scanBasePackages = {"com.wenhao"})
+/**
+ * 排除数据库连接
+ */
+@SpringBootApplication(scanBasePackages = {"com.wenhao"},
+        exclude = {DataSourceAutoConfiguration.class,
+                DataSourceTransactionManagerAutoConfiguration.class,
+                HibernateJpaAutoConfiguration.class})
 public class PortalWebApplication {
 
     public static void main(String[] args) {
